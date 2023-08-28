7a2a96c77597a1738565d4b5dc16c412ec36dc9c
sofevans <142666310+sofevans@users.noreply.github.com>
2023-08-28 10:22:16 -0400

    Pattern.java 1.3
    Assignment 1.3; Display JAVA Pattern

---
+++ Pattern.java
@@ -0,0 +1,10 @@
+/** Assignment 1.3 */
+public class Pattern {
+    public static void main(String[] args) {
+        // Display JAVA in Pattern
+        System.out.println("    J   A   V     V  A");
+        System.out.println("    J  A  A  V   V  A  A");
+        System.out.println("J   J AAAAAA  V V  AAAAAA");
+        System.out.println(" J J A      A  V  A      A");
+    } 
+}
