9db5bc4b4a97369a1c9386636f1d06a4af277ad1
sofevans <142666310+sofevans@users.noreply.github.com>
2023-08-28 09:59:20 -0400

    Welcome.java
    Assignment 1.1 Display Three Messages

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+/** Assignment 1.1 */
+public class Welcome {
+    public static void main(String[] args) {
+        // Display Welcome to Java, Welcome to Computer Science,
+        // Programming is fun! on console
+        System.out.println("Welcome to Java,");
+        System.out.println("Welcome to Computer Science,");
+        System.out.println("Programming is fun!");
+    }
+}
