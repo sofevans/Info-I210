74145627c889779f00a9c0b3a080aa76d8d564cb
sofevans <142666310+sofevans@users.noreply.github.com>
2023-09-04 23:48:54 -0400

    Create Quotient.java
    Assignment 1.5 for Info-I210

---
+++ Quotient.java
@@ -0,0 +1,10 @@
+/** Assignment 1.5; display quotient and results */
+public class Quotient {
+    public static void main (String[]args){
+        System.out.println(" 9.5 x 4.5 - 2.5 x 3");
+        System.out.print("--------------------- = ");
+        // Dotted line chosen over solid line due to spacing preference
+        System.out.println(+ (9.5 * 4.5 - 2.5 * 3.0) / (45.5 - 3.5));
+        System.out.println("     45.5 - 3.5");
+    }
+}
