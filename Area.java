5f195cbeaa54656f8946a0a7fd9ee853f0f83fcd
sofevans <142666310+sofevans@users.noreply.github.com>
2023-09-19 22:25:28 -0400

    Create Area.java
    Info-I210 Listing 2.1

---
+++ Area.java
@@ -0,0 +1,14 @@
+/** Listing 2.1 */
+public class Area {
+    public static void main(String[] args){
+        double radius; //declare radius
+        double area; //declare areaa
+        //Assign a radius
+        radius = 20; //radius is now 20
+        //Compute Area
+        area = radius * radius *3.14159;
+        //Display Results
+        System.out.println("The area for the circle of radius" + radius + 
+                " is " + area);
+    }
+}
