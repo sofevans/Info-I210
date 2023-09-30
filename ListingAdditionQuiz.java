8bf22b01de9a67e6159b1bd4a5bd16eee78ddb4d
sofevans <142666310+sofevans@users.noreply.github.com>
2023-09-30 09:32:17 -0400

    Create ListingAdditionQuiz.java
    Listing 3.1

---
+++ ListingAdditionQuiz.java
@@ -0,0 +1,14 @@
+/** Listing 3.1 */
+import java.util.Scanner;
+public class ListingAdditionQuiz {
+  public static void main(String[] args) {
+    int number1 = (int)(System.currentTimeMillis() % 10);
+    int number2 = (int)(System.currentTimeMillis() / 10 % 10);
+    // Create a Scanner
+    System input = new Scanner(System.in);
+    System.out.print(
+      "What is " + number1 " + " + number2 + " ?");
+    int answer = input.nextInt();
+    System.out.println(
+      number1 + " + " + number2 + " = " + answer + " is " +
+        (number1 + number2 == answer));
