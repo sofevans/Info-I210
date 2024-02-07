a34f946ad6be2d7ba69cc211f1783686d28f80c1
sofevans <142666310+sofevans@users.noreply.github.com>
2024-02-07 04:31:44 -0500

    Create SplittingString.java

---
+++ SplittingString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SplittingString {
+    public static void main(String[] args){
+        String[] s = {"ab#12#453", "#"};
+        String[] regex = {"a?b?gf#e", "[?#}"};
+    }
+    public static String[] split(String s, String regex) {
+        for (int i = 0; i < Array1.length; i++) {
+            for (int j = 0; j < Array2.length; j++) 
+                if (array1[i] == (array2[j])) {
+                    System.out.println("Matching elements are : " 
+                            + (array1[i]));
+                }
+        String[] Array1[i] = s.split(" ");
+        return array1[i];
