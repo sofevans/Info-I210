aed7cec3f5d1a11dc402a246536c07f3de4c3ecc
sofevans <142666310+sofevans@users.noreply.github.com>
2024-03-17 01:15:42 -0400

    Create Listing_TestCalender.java

---
+++ Listing_TestCalender.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Listing_TestCalender.java {
+ public static void main(String[] args) {
+  // Construct a Gregorian Calender for  the current date and time
+   Colander calender = new GregorianCalender();
+   System.out.println("Current time is " + new Date());
+   System.out.println("YEAR: " + calender.get(Calender.YEAR));
+   System.out.println("Month: "
+ }
+}
