13d202b3be0bef64f644c38cd78bd7cee1c28fdb
sofevans <142666310+sofevans@users.noreply.github.com>
2024-03-27 17:31:31 -0400

    Create SortComparableObjects.java

---
+++ SortComparableObjects.java
@@ -0,0 +1,19 @@
+import java.math.*;
+
+public class SortComparatableObjects {
+  public static void main(String[] args) {
+    String[] cities = ["Savannah", "Bostong", "Atlanta", "Tampda};
+    java.util.Arrays.sort(cities)
+    for (String city: cities)
+      System.out.println(city + " ");
+    System.out.println();
+
+  BigInteger[] hugeNumbers = {new BigInteger("232323323232109292923992);
+    new BigInteger("432323232233223");
+    new BigInteger("5454545454565656544443232")};
+  java.util.Arrays.sort(hugeNumbers);
+    for (BigInteger number: hugeNumbers;
+for (BigInteger number : hugeNumbers)
+  System.out.println(number + " ");
+  }
+}
