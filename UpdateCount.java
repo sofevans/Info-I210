ea3ce9aa759e5bb2fe93a1031641ede2daf854cf
sofevans <142666310+sofevans@users.noreply.github.com>
2024-04-20 08:45:17 -0400

    Create UpdateCount.java

---
+++ UpdateCount.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class UpdateCount {
+    public static void main(String[]args ) throws IOException {
+        try (
+        RandomAccessFile raf = new RandomAccessFile("Exercise17_08.dat", "rw");
+        ) {
+            int count = 0;
+            if (raf.length() > 0)
+                count = raf.readInt();
+            raf.seek(0);
+            raf.writeInt(++count);
+            System.out.println("Current count is " + count);
+        }
+    }
+}
